a7cea6f26ce18248e9ee40b4eef8c87ae3ba5060
Bruno Lima <brunolimadevelopment@gmail.com>
2023-07-05 10:35:17 -0300

    Eventos por props

--- src/components/Evento.js
+++ src/components/Evento.js
@@ -2,6 +2,7 @@
  * Os eventos no react são definidos na própria tag do HTML
  * ex: onClick, onChange e etc;
  */
+import Button from "./evento/Button";
 
                             // destructuring
 export default function Evento({ numero }) {
@@ -10,10 +11,10 @@ export default function Evento({ numero }) {
         console.log(`Opa, fui ativado! ${numero}`);
     }
 
-
     return (
         <div>
             <p>Clique para disparar um evento!</p>
+            <Button event={meuEvento} text="Primeiro Evento" />
             <button onClick={meuEvento}>Ativar!</button>
         </div>
     )
