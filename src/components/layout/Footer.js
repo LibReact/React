fec4b09cee06179b546d3eba04925b7f71a69aa9
Bruno Lima <brunolimadevelopment@gmail.com>
2023-07-06 17:27:47 -0300

    React Icons

--- src/components/layout/Footer.js
+++ src/components/layout/Footer.js
@@ -1,3 +1,15 @@
+import { FaFacebook, FaInstagram, FaLinkedin } from 'react-icons/fa';
+import styles from './Footer.module.css'
+
 export default function Footer() {
-    return (<footer>Rodape</footer>)
+    return (
+        <footer>
+            <ul className={styles.social_list}>
+                <li><FaFacebook /></li>
+                <li><FaInstagram /></li>
+                <li><FaLinkedin /></li>
+            </ul>
+            <p>Nosso rodapé</p>
+        </footer>
+    )
 }
